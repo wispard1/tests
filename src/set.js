ffe08a78e18cfa9151774ebeedf5f6251df5995f
Anton <antonvlasov610@gmail.com>
2025-04-11 16:49:09 +0800

    update

--- src/set.js
+++ src/set.js
@@ -1,7 +1,10 @@
-const set = (obj, key, value) => {  
-    if (!(key in obj)) {
-      obj[key] = value;
-    }
-  };
+const set = (obj, key, value) => {
+  if (typeof obj !== 'object' || obj === null) {
+    throw new Error('Первый аргумент должен быть объектом');
+  }
+  if (!(key in obj)) {
+    obj[key] = value;
+  }
+};
 
-  export default set;
+export default set;
--- tests/set.test.js
+++ tests/set.test.js
@@ -1,15 +1,14 @@
+import { strict as assert } from 'node:assert';
+
 import set from '../src/set.js';
 
 const obj = {};
 
 set(obj, 'key1', 'value1');
-if (obj.key1 !== 'value1') {
-  throw new Error('Функция работает неверно!');
-}
+
+assert.strictEqual(obj.key1, 'value1', 'Свойство key1 должно быть установлено в "value1"');
 
 set(obj, 'key1', 'next value');
-if (obj.key1 !== 'next value') {
-  throw new Error('Функция работает неверно!');
-}
+assert.strictEqual(obj.key1, 'value1', 'свойство key1 не должно быть перезаписано');
 
 console.log('Все тесты пройдены!');
